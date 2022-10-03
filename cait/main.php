93250cdfbe8bb3a5938488709b5cc1fe81421a59
BlaastX <114914833+BlaastX@users.noreply.github.com>
2022-10-03 14:08:07 +0400

    Add files via upload

---
+++ cait/main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Главная</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="logotip" src="img/logomal.png" alt="">
+    <div class="header">    
+        <h2 class="mainurl">Главная <img class="litlog" src="img/mainlog.png" ></h2>
+        <h2 class="profurl">Личный кабинет<img class="litlog" src="img/proflog.png"></h2>
+    </div>
+    <img class="foto" src="img/foto.png">
+    <h2>Ваши заявки</h2>
+    <h2><a href="">Создать заявку</a></h2>
+
+</body>
+</html>
